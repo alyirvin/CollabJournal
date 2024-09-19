8d8c712b703aab7f93e80d4db0ae16aa6f9f355e
alyirvin <123898005+alyirvin@users.noreply.github.com>
2024-09-19 18:10:19 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import logo from './logo.svg';
+import './App.css';
+import axios from 'axios';
+import React from 'react';
+
+function App() {
+  const [test, setTest] = React.useState(false);
+  const [data, setData] = React.useState('');
+
+  async function getBackendCall() {
+    const response = await axios.get('http://localhost:5008/test');
+    console.log(response.data);
+    setData(response.data);
+  }
+
+  async function handleClick() {
+    await getBackendCall();
+    setTest(!test);
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        {test && <p>{data}</p>}
+        <button onClick={handleClick}>Click me</button>
+      </header>
+    </div>
+
+  );
+}
+
+export default App;
