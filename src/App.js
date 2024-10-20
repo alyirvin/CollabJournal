67fc1ce07acf24aa385cc688efdd1761919783de
alyirvin <alysha.irvin@gmail.com>
2024-10-20 15:03:51 -0400

    Added routing

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Landing from './Components/LoginSignup/Landing';
 import Signup from './Components/LoginSignup/Signup';
 import Login from './Components/LoginSignup/Login';
 import Forgot from './Components/LoginSignup/Forgot';
+<<<<<<< Updated upstream
 import Groups from './Components/LoginSignup/Groups';
 function App() {
   return (
@@ -12,6 +13,20 @@ function App() {
       {/* <Login/> */}
       {/* <Forgot/> */}
       <Groups/>
+=======
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+function App() {
+  return (
+    <div>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Landing/>} />
+          <Route path="/Login" element={<Login/>} />
+          <Route path="/Signup" element={<Signup/>} />
+        </Routes>
+      </Router>
+>>>>>>> Stashed changes
     </div>
   );
 }
