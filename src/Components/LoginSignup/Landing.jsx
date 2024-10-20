8e1ed51aa5a2614e3facd92c64e816dce8765889
c-mfy <xinyusu06@gmail.com>
2024-10-20 15:34:12 -0400

    Fixed Landing page css
    +added ".landingContainer" in front of each css block to make it unique

--- src/Components/LoginSignup/Landing.jsx
+++ src/Components/LoginSignup/Landing.jsx
@@ -8,7 +8,7 @@ import { Link } from 'react-router-dom';
 const Landing = () => {
 
     return (
-        <div>
+        <div className = "landingContainer">
 
             <div className = "spiral-container">
 
