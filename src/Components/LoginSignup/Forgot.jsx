7d8a86cf0a50a9eb43826b9e1e094f25c8ed2ef3
Summer McRoy <summernmcroy@gmail.com>
2024-10-20 15:20:12 -0400

    Forgot Pass and Reset Pass changes
    Updated forgot pass and reset pass pages.

--- src/Components/LoginSignup/Forgot.jsx
+++ src/Components/LoginSignup/Forgot.jsx
@@ -1,9 +1,27 @@
 import React, {useState} from 'react';
 import axios from 'axios';
+import './forgot.css';
+import spirals from './../../images/spirals.png';
+
 const Forgot = () => {
     return(
-        <div>
-            <a href='./forgot.jsx'>Click to navigate</a> 
+        <div className="page">
+            <div className="containerSpiral">
+                <img src={spirals} alt="spiral" id="spiral"/>
+            </div>
+            <p className="title">FORGOT PASSWORD</p>
+            <label className="textOne">
+            We'll send you a link to the email address you registered with.
+            </label>
+        <div className="containerOne">
+        <label className="textTwo">Email</label>
+        <input className="input" />
+        </div>
+        <button className="button">
+            <a className="button-prop" href="reset.jsx">
+                <div id="button-text">SEND EMAIL</div>
+            </a>
+        </button>
         </div>
     )
 }
