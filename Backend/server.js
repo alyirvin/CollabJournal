f06c2bd26ba945054d9350e649e89250d7a462e9
alyirvin <123898005+alyirvin@users.noreply.github.com>
2024-10-20 21:48:10 -0400

    Updated to match Backend branch

--- Backend/server.js
+++ Backend/server.js
@@ -2,7 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const userCollection = require('./src/mongodb.js')
+const userCollection = require('./mongodb.js')
+const bcrypt = require('bcrypt');
 mongoose.connect("mongodb+srv://test_ili:justtesting@collaborativejournal.itlf7.mongodb.net/?retryWrites=true&w=majority&appName=CollaborativeJournal")
 .then(() => {
     console.log("mongodb connected");
@@ -15,31 +16,49 @@ app.use(cors());
 app.use(bodyParser.json());
 app.post('/Signup', async (req, res) => {
     const {user, email, password} = req.body;
-    const newUser = new userCollection({
-        name: user,
-        email: email,
-        password: password
-    })
-    await newUser.save();
-    res.send('User added successfully');
+
+    try {
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const newUser = new userCollection({
+            name: user,
+            email: email,
+            password: hashedPassword
+        });
+
+        await newUser.save();
+
+        res.send('User added successfully');
+    } catch(error) {
+        res.send('Hashing failed');
+    }
   });
 
 app.post('/Login', async (req, res) => {
     const {user, email, password} = req.body;
     try {
-        const check = await userCollection.findOne({name: user, email: email, password: password})
+        const check = await userCollection.findOne({name: user, email: email});
         if(check) {
-            res.send("Exist")
+            try {
+                const checkPassword = await bcrypt.compare(password, check.password);
+                if(checkPassword) {
+                    res.send("Exist") 
+                }
+                else {
+                    res.send("Wrong Password")
+                }
+            }
+            catch(e) {
+                res.send("Wrong Password")
+            }
         }
         else {
-            res.send("Not found")
+            res.send("UserEmail not found")
         }
     }
     catch(e) {
-        res.send("error")
+        res.send("UserEmail not found")
     }
 })
 app.listen(5001, () => {
     console.log('Server is running on port 5001')
 })
-//TODO verify what kind of error the login info has
