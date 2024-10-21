0c1ac97bb92ac37f0f957abdeac978e865e54aaf
Islam, Marziah <mislam302@gatech.edu>
2024-10-20 23:25:55 -0400

    fixing MiniJournal spirals

--- Frontend/src/Components/Groups.jsx
+++ Frontend/src/Components/Groups.jsx
@@ -1,7 +1,6 @@
 import React, {useState} from 'react';
 import axios from 'axios';
 import './groups.css';
-import spiralSmall from './images/spiral-bind-short.png';
 import MiniJournal from './MiniJournal'
 const Groups = () => {
     return(
