20cfe672a5eadd113acdfb552cee2dc57c29ffa3
alyirvin <123898005+alyirvin@users.noreply.github.com>
2024-10-22 02:46:49 -0400

    Merging backend's frontend additions part 1

--- Frontend/src/Components/Forgot.jsx
+++ Frontend/src/Components/Forgot.jsx
@@ -1,9 +1,22 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 import './forgot.css';
 import spirals from './images/spirals.png';
 
 const Forgot = () => {
+    const [email, setEmail] = useState('');
+    const [message, setMessage] = useState('');
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await axios.post('http://localhost:5001/ForgotPassword', { email });
+            setMessage(response.data);
+        } catch (error) {
+            setMessage(error.response.data);
+        }
+    };
+    
     return(
         <div className="forgotContainer">
             <div className="containerSpiral">
